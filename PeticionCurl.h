5ad4c7d74e217481f8213363b6354c6b059d9879
RiverA0214 <joanrva0214@gmail.com>
2025-03-27 21:59:02 -0500

    Ahora si

---
+++ PeticionCurl.h
@@ -0,0 +1,14 @@
+#ifndef PETICIONCURL_H
+#define PETICIONCURL_H
+#include <string>
+#include <iostream>
+  
+class PeticionCurl{
+    private:
+        
+    public:
+        void PedirCurl();//se encargara de hacer la peticion a la api de validar numero telefonico
+        
+}
+
+#endif // PETICIONCURL_H
